8b330c326d2c85ef169dc10efdcd23152a495bb2
Kishore <abyu.kishore@gmail.com>
2014-07-09 11:02:34 +0530

    test working of js hooking

--- app/assets/javascripts/d2d_sync.js
+++ app/assets/javascripts/d2d_sync.js
@@ -1,6 +1,8 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-$('.google_signin').click(function() {
-	console.log('hi');
+$(document).ready(function () {
+$('.google-signin').bind('click', function() {
+	alert('hi')
+});
 });
